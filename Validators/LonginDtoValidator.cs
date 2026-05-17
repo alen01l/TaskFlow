097b23f8f78de8a12e0be720807d57b0fcaea694
alen01l <168632308+alen01l@users.noreply.github.com>
2026-05-17 15:59:56 +0200

    validate auth contracts

---
+++ Validators/LonginDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using TaskFlow.Api.Contracts.Auth;
+
+namespace TaskFlow.Api.Validators;
+
+public class LoginDtoValidator : AbstractValidator<LoginDto>
+{
+    public LoginDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty();
+    }
+}
---
+++ Validators/RegisterDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using TaskFlow.Api.Contracts.Auth;
+
+namespace TaskFlow.Api.Validators;
+
+public class RegisterDtoValidator : AbstractValidator<RegisterDto>
+{
+    public RegisterDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(6);
+    }
+}
